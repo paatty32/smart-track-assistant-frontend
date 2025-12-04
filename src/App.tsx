fe6c3cce95761aae58775b565ee574e8736d391f
Patrick <patrick.boadu.boafo@hotmail.de>
2025-12-04 02:10:47 +0100

    add - init start project with react + tailwindcss + heroui

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import {Card, CardBody, } from "@heroui/react";
+
+export default function App() {
+
+  return (
+    <Card className="max-w-[340px]">
+      <CardBody>
+        Hello World
+      </CardBody>
+    </Card>
+  );
+}
+
